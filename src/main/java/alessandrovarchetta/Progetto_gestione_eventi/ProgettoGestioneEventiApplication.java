39c61a2849fc1e894c4932a8ea583cbf12bf0c92
alessandrovarc-git <alessandro.varchetta1996@gmail.com-git>
2024-11-08 10:53:53 +0100

    Initial commit

---
+++ src/main/java/alessandrovarchetta/Progetto_gestione_eventi/ProgettoGestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package alessandrovarchetta.Progetto_gestione_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoGestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoGestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessandrovarchetta/Progetto_gestione_eventi/ProgettoGestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package alessandrovarchetta.Progetto_gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoGestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
